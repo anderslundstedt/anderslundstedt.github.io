2033a0a3430b36716eee09cbc0335f38bc16a422
Anders Lundstedt <git@anderslundstedt.se>
2019-04-19 00:02:19 +0200

    filters for logs and exceptions

---
+++ _plugins/my_filters.rb
@@ -0,0 +1,21 @@
+# Modifed the example from https://talk.jekyllrb.com/t/how-to-properly-indicate-an-error-during-site-generation/447/2
+
+module MyFilters
+    def log_warning(msg)
+        bad_file = @context.registers[:page]['path']
+        err_msg = "\033[33mIn #{bad_file}: #{msg}\033[0m"
+        warn err_msg
+        return msg
+    end
+    def log_error(msg)
+        bad_file = @context.registers[:page]['path']
+        err_msg = "\033[31mIn #{bad_file}: #{msg}\033[0m"
+        warn err_msg
+        return msg
+    end
+    def raise_error(msg)
+        raise msg
+    end
+end
+
+Liquid::Template.register_filter(MyFilters)
