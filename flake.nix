d1246352be2bb6d07589a89f9829213e7c434adf
Anders Lundstedt <git@anderslundstedt.se>
2026-05-28 14:32:35 +0200

    update flake.nix and flake.lock
    I want to keep the inputs in sync with my system configuration

--- flake.nix
+++ flake.nix
@@ -1,13 +1,13 @@
 {
   description = "A GitHub Pages development environment";
 
-  inputs.nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+  inputs.nixpkgs-unstable.url = "nixpkgs/nixpkgs-unstable";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = {self, nixpkgs, flake-utils}:
+  outputs = {self, nixpkgs-unstable, flake-utils}:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; }; in
+      let pkgs = import nixpkgs-unstable { inherit system; }; in
       {
         devShell = pkgs.mkShell {
             buildInputs = [
