bf7b2581456ae3451ee02866dc6b69925c6957c4
Anders Lundstedt <git@anderslundstedt.se>
2026-08-24 09:01:36 +0200

    sync flake inputs with my nix-pins repo

--- flake.nix
+++ flake.nix
@@ -1,11 +1,13 @@
 {
   description = "A GitHub Pages development environment";
 
-  inputs.nixpkgs-unstable.url = "nixpkgs/nixpkgs-unstable";
+  inputs.pins.url = "github:anderslundstedt/nix-pins";
+
+  inputs.nixpkgs-unstable.follows = "pins/nixpkgs-unstable";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = {self, nixpkgs-unstable, flake-utils}:
+  outputs = {nixpkgs-unstable,flake-utils,...}:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = import nixpkgs-unstable { inherit system; }; in
       {
